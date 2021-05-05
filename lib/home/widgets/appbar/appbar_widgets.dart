ad732c92366fe7d8a15c679802510482dd215f88
Ney Robson <ney.robson@kbank.com.br>
2021-05-05 08:51:19 -0300

    first commit

---
+++ lib/home/widgets/appbar/appbar_widgets.dart
@@ -0,0 +1,45 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widegets.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "Ney Robson",
+                                  style: AppTextStyles.titleBold)
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(29),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/64109513?v=4"))),
+                        )
+                      ],
+                    ),
+                    ScoreCardWidget()
+                  ],
+                ),
+              ),
+            ));
+}
